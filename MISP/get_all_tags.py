afceeb79dbcf776e480f4bfc9227475a86b581b7
Azeros <11859951+Az0res@users.noreply.github.com>
2021-07-06 13:19:58 +0200

    Create get_all_tags.py

---
+++ MISP/get_all_tags.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from pymisp import ExpandedPyMISP
+from keys import misp_url, misp_key, misp_verifycert
+import argparse
+import json
+
+def get_tags(m):
+    result = m.get_all_tags(True)
+    r = result
+    print(json.dumps(r) + '\n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get tags from MISP instance.')
+
+    args = parser.parse_args()
+    try:
+        misp = ExpandedPyMISP(misp_url, misp_key, misp_verifycert)
+        tags = misp.tags(pythonify=True)
+
+        for tag in tags:
+
+            print(tag["name"])
+
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
