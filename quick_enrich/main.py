314b89d2362d3a0a3351d3429fb0752a9e6e755f
azo <javier.lethe@gmail.com>
2022-05-12 09:52:29 +0200

    qsdf

--- quick_enrich/main.py
+++ quick_enrich/main.py
@@ -2,30 +2,36 @@
 # Requierments: pip3 install "fastapi[all]" pyasn
 # run with uvicorn fastAPI:app --reload
 
-# TODO manage proxy for outgoing requests
-# todo: ad parameter for non-local queries saying I know it might leak data and trigger iocs
+#todo: ad parameter for non-local queries saying I know it might leak data and trigger iocs
 #todo: add tranco list top 10k
 import asyncio
 import ipaddress
 import operator
 import socket
 from functools import reduce
+import requests
 
 from fastapi import FastAPI
 import pyasn
 from typing import Optional
 
 app = FastAPI()
-
-asndb = pyasn.pyasn(
-    '/home/azo/.asn_data/converted.ipasn',
-    as_names_file='/home/azo/.asn_data/asnames.json',
-)
+####################################################################
+# INIT VARS
+###################################################################
+VT_API_KEY = ""
+####################################################################
+# INIT OBJECTS
+####################################################################
+asndb = pyasn.pyasn('/home/azo/.asn_data/asn_numbers.ipasn', as_names_file='/home/azo/.asn_data/asnames.json',)
 tor_ips = set()
 for line in open("/home/azo/.asn_data/exit-addresses"):
     if "ExitAddress" in line:
         _, ip, _ = line.split(" ", 2)
         tor_ips.add(ip)
+vt_session = requests.Session()
+vt_session.headers = {'X-Apikey': VT_API_KEY}
+###################################################################
 
 @app.get("/fast")
 async def get_all_fast(ip: str ):
@@ -52,6 +58,19 @@ async def get_is_tor_exit(ip: str ):
 async def get_reverse_dns(ip: str ):
     return {"ip": ip, "dns": socket.gethostbyaddr(ip)[0]}
 
+@app.get("/slow/sha256/vt")
+async def get_vt_results(hash:str):
+    url = "https://www.virustotal.com/api/v3/monitor_partner/hashes/"+hash+"/items"
+    return {hash: vt_session.get(url).json()}
+
 @app.get("/")
 async def root():
     return {"message": "go to /docs and RTFM"}
+
+
+
+# #download asn_data:
+# pyasn_util_download.py --latestv46
+# pyasn_util_convert.py --single <Downloaded RIB File> <ipasn_db_file_name>
+# pyasn_util_asnames.py > asname.json
+# wget https://check.torproject.org/exit-addresses
--- yfinance_get_data.py
+++ yfinance_get_data.py
@@ -4,20 +4,52 @@ from time import sleep
 import pandas as pd
 import yfinance as yf
 
+ticker = "AAPL"
 
-ticker="AAPL"
-
-
-#we start 5 years ago
+# we start 5 years ago
 year = timedelta(days=365)
 years_ago = year * 5
-INTERVAL = timedelta(days= 50)
+DOWNLOAD_INTERVAL = timedelta(days=55)
+TICKER_DATA_INTERVAL = "90m"
 
 START_DATE_d = date.today() - years_ago
-END_DATE_d = START_DATE_d + INTERVAL
+END_DATE_d = START_DATE_d + DOWNLOAD_INTERVAL
 
-while END_DATE_d < date.today():
-    df = yf.download(ticker, start=START_DATE_d.strftime("%Y-%m-%d"), end=END_DATE_d.strftime("%Y-%m-%d"), interval="90m")
-    START_DATE_d = END_DATE_d
-    END_DATE_d = START_DATE_d + INTERVAL
-    sleep(2)
+def get_column_from_csv(file, col_name):
+    # Try to get the file and if it doesn't exist issue a warning
+    try:
+        df = pd.read_csv(file)
+    except FileNotFoundError:
+        print("File Doesn't Exist")
+    else:
+        return df[col_name]
+
+
+# get all tickers
+#tickers = get_column_from_csv("D:/Python for Finance/Wilshire-5000-Stocks.csv", "Ticker")
+tickers=["AAPL","GOOG","TSL"]
+print(len(tickers))
+failed_tickers=[]
+
+for t in tickers:
+    the_file = "tickers" + ticker.replace(".", "_") + '.csv'
+    try:
+        print("Get Data for : ", ticker)
+        stock = yf.Ticker(ticker)
+        # get all data for each ticker
+        tmp_st_date=START_DATE_d
+        tmp_end_date=END_DATE_d
+
+        while tmp_end_date < date.today():
+            df = stock.history(ticker, start=tmp_st_date.strftime("%Y-%m-%d"), end=tmp_end_date.strftime("%Y-%m-%d"),
+                               interval=TICKER_DATA_INTERVAL)
+            tmp_st_date = tmp_end_date
+            tmp_end_date = tmp_st_date + DOWNLOAD_INTERVAL
+            df.to_csv(the_file, mode='a')
+            sleep(2)
+        print(the_file, " Saved")
+    except Exception as e:
+        print("Could not download ticker {}: \t{}".format(t, e))
+        failed_tickers.append(t)
+
+print(failed_tickers)
